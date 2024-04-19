bc345fc875f0fd34ae272d609e73716a05bc883e
SangHitam77 <167504333+SangHitam77@users.noreply.github.com>
2024-04-19 14:33:09 +0800

    Create Blok.py

---
+++ Blok.py
@@ -0,0 +1,18 @@
+# Blokfb
+import os 
+import requests
+import sys
+import time 
+def dareng(s):
+  for c in s +'\n':
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(1. /50)
+
+def menu ():
+  print ('\x1b[4;91m \x1b[4;00m'*40m)
+  dareng (' PLEASE YOU HAVE TO LOG IN ')
+  print ('\33[1;37mENTER THE NAME \33[1;33mFARDI\33[1;32m DALITA \33[1;37mTO LOGIN')
+  print ('\x1b[4;91m \x1b[4;00m')
+  print (' ')
+  input = 
