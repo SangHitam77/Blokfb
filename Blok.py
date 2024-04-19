0c8561fbfe908fea9f1aeeb875067c2f2217c3f1
SangHitam77 <167504333+SangHitam77@users.noreply.github.com>
2024-04-19 14:44:48 +0800

    Update Blok.py

--- Blok.py
+++ Blok.py
@@ -11,8 +11,10 @@ def dareng(s):
 
 def menu ():
   print ('\x1b[4;91m \x1b[4;00m'*40m)
-  dareng (' PLEASE YOU HAVE TO LOG IN ')
+  dareng ('\33[1;37m PLEASE YOU HAVE TO LOG IN ')
   print ('\33[1;37mENTER THE NAME \33[1;33mFARDI\33[1;32m DALITA \33[1;37mTO LOGIN')
-  print ('\x1b[4;91m \x1b[4;00m')
+  print ('\x1b[4;91m \x1b[4;00m'*40)
   print (' ')
-  input = 
+  areng=str(input('\33[1;32mPLEASE ENTER THE NAME ABOVE :\33[1;36m')
+  if areng=='fardi dalita':
+     dareng ('
